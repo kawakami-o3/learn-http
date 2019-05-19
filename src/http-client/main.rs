6a9b39df1741c966c6cb1ced7233408e871a4489
kawakami <kawakami.ozone@gmail.com>
2019-05-19 16:16:06 +0900

    Add an example of an echo server end client.

--- src/http-client/main.rs
+++ src/http-client/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::prelude::*;
+use std::net::TcpStream;
+
+fn main() -> std::io::Result<()> {
+    let mut stream = TcpStream::connect("127.0.0.1:34254")?;
+
+    stream.write(&[10])?;
+    let mut buf = vec![0; 128];
+    stream.read(&mut buf)?;
+    println!("> {:?}", buf);
+    Ok(())
 }
--- src/http-server/main.rs
+++ src/http-server/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
+fn handle_client(stream: &mut TcpStream) {
+    println!("> {:?}", stream);
+    //let mut buf = [0; 128];
+    let mut buf = vec![0; 128];
+    match stream.read(&mut buf) {
+        Ok(c) => {
+            println!("ok : {:?} {:?}", c, buf);
+        }
+        Err(e) => {
+            println!("err: {:?}", e);
+        }
+    }
+    stream.write(&buf).unwrap();
+}
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:34254")?;
+
+    for stream in listener.incoming() {
+        handle_client(&mut stream?);
+    }
+    Ok(())
 }
