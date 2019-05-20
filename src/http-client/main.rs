1102ff69b3f1ed99a5c770d95083dde76b691d53
kawakami <kawakami.ozone@gmail.com>
2019-05-21 04:26:47 +0900

    Change a client to retrieve a whole message.

--- src/http-client/main.rs
+++ src/http-client/main.rs
@@ -2,24 +2,42 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 
 fn main() -> std::io::Result<()> {
-    let host = "127.0.0.1:34254";
+    //let host = "127.0.0.1:34254";
+    let host = "www.google.com:80";
     let mut stream = TcpStream::connect(host)?;
 
     // Ex.) curl 127.0.0.1:34254 --http1.0
     //let cnt = format!("GET / HTTP/1.0\r\nHost: {}\r\nUser-Agent: curl/7.58.0\r\nAccept: */*\r\n\r\n", host);
 
     // Simple-Request
-    let cnt = format!("GET http://{}/index.html\r\n", host);
+    //let cnt = format!("GET http://{}/\r\n", host);
+    //let cnt = format!("GET http://www.google.com/\r\n", host);
+    let cnt = format!("GET http://www.google.com/\r\n");
+    //let cnt = format!("GET /\r\n");
 
     stream.write(cnt.as_bytes())?;
-    let mut buf = vec![0; 128];
-    match stream.read(&mut buf) {
-        Ok(n) => {
-            println!("> {} {}", n, String::from_utf8(buf[0..n].to_vec()).unwrap());
-        }
-        Err(e) => {
-            println!("{:?}", e);
+    //let mut buf = vec![0; 1024*1024*1024];
+    let mut buf = vec![0; 1024];
+    let mut response = Vec::new();
+    loop {
+        match stream.read(&mut buf) {
+            Ok(n) => {
+                //println!("> {} {}", n, String::from_utf8(buf[0..n].to_vec()).unwrap());
+                //print!("{}", String::from_utf8(buf[0..n].to_vec()).unwrap());
+                response.append(&mut buf[0..n].to_vec());
+                if n == 0 {
+                    break;
+                }
+            }
+            Err(e) => {
+                println!("{:?}", e);
+            }
         }
     }
+
+    unsafe {
+        println!("{}", String::from_utf8_unchecked(response));
+    }
+    //println!("{}", String::from_utf8(response).unwrap());
     Ok(())
 }
