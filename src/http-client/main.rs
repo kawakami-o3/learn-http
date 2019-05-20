dcb8c942b13cb262b8e5858fdf1f89cf0c8bf9f7
kawakami <kawakami.ozone@gmail.com>
2019-05-20 22:22:27 +0900

    Add an example of Simple Request.

--- src/http-client/main.rs
+++ src/http-client/main.rs
@@ -2,11 +2,24 @@ use std::io::prelude::*;
 use std::net::TcpStream;
 
 fn main() -> std::io::Result<()> {
-    let mut stream = TcpStream::connect("127.0.0.1:34254")?;
+    let host = "127.0.0.1:34254";
+    let mut stream = TcpStream::connect(host)?;
 
-    stream.write(&[10])?;
+    // Ex.) curl 127.0.0.1:34254 --http1.0
+    //let cnt = format!("GET / HTTP/1.0\r\nHost: {}\r\nUser-Agent: curl/7.58.0\r\nAccept: */*\r\n\r\n", host);
+
+    // Simple-Request
+    let cnt = format!("GET http://{}/index.html\r\n", host);
+
+    stream.write(cnt.as_bytes())?;
     let mut buf = vec![0; 128];
-    stream.read(&mut buf)?;
-    println!("> {:?}", buf);
+    match stream.read(&mut buf) {
+        Ok(n) => {
+            println!("> {} {}", n, String::from_utf8(buf[0..n].to_vec()).unwrap());
+        }
+        Err(e) => {
+            println!("{:?}", e);
+        }
+    }
     Ok(())
 }
--- src/http-server/main.rs
+++ src/http-server/main.rs
@@ -1,24 +1,29 @@
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 
+const BUF_SIZE: usize = 1024;
+
+const HOST: & str = "127.0.0.1:34254";
+
 fn handle_client(stream: &mut TcpStream) {
-    println!("> {:?}", stream);
     //let mut buf = [0; 128];
-    let mut buf = vec![0; 128];
+    let mut buf = vec![0; BUF_SIZE];
     match stream.read(&mut buf) {
-        Ok(c) => {
-            println!("ok : {:?} {:?}", c, buf);
+        Ok(n) => {
+            println!("ok : {}\n{}", n, String::from_utf8(buf[0..n].to_vec()).unwrap());
+            stream.write(&buf[0..n]).unwrap();
         }
         Err(e) => {
             println!("err: {:?}", e);
+            stream.write("error\r\n".as_bytes()).unwrap();
         }
     }
-    stream.write(&buf).unwrap();
 }
 
 fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:34254")?;
+    let listener = TcpListener::bind(HOST)?;
 
+    println!("starting...");
     for stream in listener.incoming() {
         handle_client(&mut stream?);
     }
