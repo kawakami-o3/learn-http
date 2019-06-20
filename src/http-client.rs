844bc8ecaab26cc828e6d3f9b7f4df7c1506e2fb
kawakami <kawakami.ozone@gmail.com>
2019-06-20 22:20:59 +0900

    Refactor client request.

--- src/http-client.rs
+++ src/http-client.rs
@@ -1,6 +1,25 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
 
+fn request_body() -> String {
+    format!("GET /get HTTP/1.0\r\n\r\n")
+
+    // Simple-Request
+    //format!("GET http://{}/\r\n", host);
+    //format!("GET http://www.google.com/\r\n", host);
+    //format!("GET http://www.google.com/\r\n");
+    //format!("GET /index.html\r\n");
+    //format!("GET /index.html HTTP/1.0\r\n\r\n");
+
+    // HTTP/1.0
+    //format!("GET / HTTP/1.0\r\nPragma: no-cache\r\n\r\n");
+    //format!("GET /ip HTTP/1.0\r\nPragma: no-cache\r\n\r\n");
+    //format!("GET /get HTTP/1.0\r\n\r\n");
+
+    // HTTP/1.0
+    //format!("GET /index.html HTTP/1.1\r\n\r\n");
+}
+
 fn main() -> std::io::Result<()> {
     //let host = "127.0.0.1:34254";
     //let host = "www.google.com:80";
@@ -10,16 +29,8 @@ fn main() -> std::io::Result<()> {
     // Ex.) curl 127.0.0.1:34254 --http1.0
     //let cnt = format!("GET / HTTP/1.0\r\nHost: {}\r\nUser-Agent: curl/7.58.0\r\nAccept: */*\r\n\r\n", host);
 
-    // Simple-Request
-    //let cnt = format!("GET http://{}/\r\n", host);
-    //let cnt = format!("GET http://www.google.com/\r\n", host);
-    //let cnt = format!("GET http://www.google.com/\r\n");
-    //let cnt = format!("GET /index.html\r\n");
-    //let cnt = format!("GET /index.html HTTP/1.0\r\n\r\n");
-    let cnt = format!("GET / HTTP/1.0\r\nPragma: no-cache\r\n\r\n");
-    //let cnt = format!("GET /index.html HTTP/1.1\r\n\r\n");
 
-    stream.write(cnt.as_bytes())?;
+    stream.write(request_body().as_bytes())?;
     //let mut buf = vec![0; 1024*1024*1024];
     let mut buf = vec![0; 1024];
     let mut response = Vec::new();
