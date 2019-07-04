9dc2997a8ce9b8e6ae708e283aec789a9facc977
kawakami <kawakami.ozone@gmail.com>
2019-07-04 23:09:38 +0900

    Add Reason-Phrase to Status-Code.

--- src/http_response.rs
+++ src/http_response.rs
@@ -2,23 +2,27 @@
 use crate::http_request::*;
 
 pub mod status {
-    pub type Code = isize;
+    pub type Code = (isize, &'static str);
 
-    pub const OK: Code = 200;
-    pub const CREATED: Code = 201;
-    pub const ACCEPTED: Code = 202;
-    pub const NO_CONTENT: Code = 204;
-    pub const MOVED_PERMANENTLY: Code = 301;
-    pub const MOVED_TEMPORARILY: Code = 302;
-    pub const NOT_MODIFIED: Code = 304;
-    pub const BAD_REQUEST: Code = 400;
-    pub const UNAUTHORIZED: Code = 401;
-    pub const FORBIDDEN: Code = 403;
-    pub const NOT_FOUND: Code = 404;
-    pub const INTERNAL_SERVER_ERROR: Code = 500;
-    pub const NOT_IMPLEMENTED: Code = 501;
-    pub const BAD_GATEWAY: Code = 502;
-    pub const SERVICE_UNAVAILABLE: Code = 503;
+    pub fn to_string(c: Code) -> String {
+        format!("{} {}", c.0, c.1)
+    }
+
+    pub const OK: Code = (200, "OK");
+    pub const CREATED: Code = (201, "Created");
+    pub const ACCEPTED: Code = (202, "Accepted");
+    pub const NO_CONTENT: Code = (204, "No Content");
+    pub const MOVED_PERMANENTLY: Code = (301, "Moved Permanently");
+    pub const MOVED_TEMPORARILY: Code = (302, "Moved Temporarily");
+    pub const NOT_MODIFIED: Code = (304, "Not Modified");
+    pub const BAD_REQUEST: Code = (400, "Bad Request");
+    pub const UNAUTHORIZED: Code = (401, "Unauthorized");
+    pub const FORBIDDEN: Code = (403, "Forbidden");
+    pub const NOT_FOUND: Code = (404, "Not Found");
+    pub const INTERNAL_SERVER_ERROR: Code = (500, "Internal Server Error");
+    pub const NOT_IMPLEMENTED: Code = (501, "Not Implemented");
+    pub const BAD_GATEWAY: Code = (502, "Bad Gateway");
+    pub const SERVICE_UNAVAILABLE: Code = (503, "Service Unavailable");
 }
 
 #[derive(Clone, Debug)]
@@ -39,7 +43,7 @@ impl Response {
         let mut ret = String::new();
 
         // Status-Line
-        ret.push_str(format!("HTTP/{} {} \r\n", self.version.to_string(), self.status).as_str());
+        ret.push_str(format!("HTTP/{} {}\r\n", self.version.to_string(), status::to_string(self.status)).as_str());
 
         ret.push_str("\r\n");
         return Vec::from(ret.as_bytes());
