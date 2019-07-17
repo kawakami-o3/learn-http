a01742de445eb74b869a7e8e0bec5a7c7312ae75
kawakami <kawakami.ozone@gmail.com>
2019-07-17 18:23:49 +0900

    Refactor.

--- src/util.rs
+++ src/util.rs
@@ -41,18 +41,27 @@ pub fn extension(target: &String) -> Option<&str> {
 }
 
 pub fn modified(target: &String) -> Result<DateTime<Utc>, String> {
+
+    fs::metadata(target)
+        .map_err(|e| e.to_string())
+        .and_then(|m| m.modified().map_err(|e| e.to_string()))
+        .and_then(|t| t.duration_since(SystemTime::UNIX_EPOCH).map_err(|e| e.to_string()))
+        .and_then(|n| Ok(DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(n.as_secs() as i64, 0), Utc)))
+
+    /*
     match fs::metadata(target) {
         Ok(metadata) => {
             match metadata.modified() {
                 Ok(time) => {
                     match time.duration_since(SystemTime::UNIX_EPOCH) {
                         Ok(n) => Ok(DateTime::<Utc>::from_utc(NaiveDateTime::from_timestamp(n.as_secs() as i64, 0), Utc)),
-                        Err(e) => Err(format!("{}", e)),
+                        Err(e) => Err(e.to_string()),
                     }
                 }
-                Err(e) => Err(format!("{}", e)),
+                Err(e) => Err(e.to_string()),
             }
         }
-        Err(e) => Err(format!("{}", e)),
+        Err(e) => Err(e.to_string()),
     }
+    */
 }
