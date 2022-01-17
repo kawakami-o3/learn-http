2d3ba09f2045cb08a333b89e160b9712a4d9299e
Hayato Mikami <kawakami.ozone@gmail.com>
2022-01-17 16:54:38 +0900

    Add request methods.

--- src/method.rs
+++ src/method.rs
@@ -3,3 +3,11 @@ pub type Method = &'static str;
 pub const GET: Method = "GET";
 pub const HEAD: Method = "HEAD";
 pub const POST: Method = "POST";
+
+// NOT IMPLEMENTED
+pub const PUT: Method = "PUT";
+pub const DELETE: Method = "DELETE";
+pub const CONNECT: Method = "CONNECT";
+pub const OPTIONS: Method = "OPTIONS";
+pub const TRACE: Method = "TRACE";
+pub const PATCH: Method = "PATCH";
